82a508e08ac6d13af0d272fadce94428d73e8e99
erakki27 <erakki27@gmail.com>
2025-02-02 23:24:18 +0530

    Final Test

--- tests/LinkedinProfileUpdate.spec.js
+++ tests/LinkedinProfileUpdate.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 import {POManager} from '../pages/POManager.page';
 
 test.describe('Linkedin Profile Update Automation', () => {
-    test('Update',  async({ page }) =>{
+    test.skip('Update',  async({ page }) =>{
 
         const poManager = new POManager(page);
         const linkedinPage = poManager.getLinkedinPage();
@@ -12,6 +12,31 @@ test.describe('Linkedin Profile Update Automation', () => {
         await linkedinPage.EnterEmail('erakki27@gmail.com');
         await linkedinPage.EnterPwd('Shutup@27a');
         await linkedinPage.ClickOnSignin();
+        await linkedinPage.ClickOnProfile();
+        await linkedinPage.ClickOnedit();
+        await linkedinPage.SaveProfile();
+        await linkedinPage.Success();
+        await page.close();
+
+    })
+
+    const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36';
+
+    test.use({
+        userAgent: 'ua',
+        storageState : 'Config/Linkedin.json'
+    });
+
+    test('Update with Auth',  async({ page }) =>{
+
+        const poManager = new POManager(page);
+        const linkedinPage = poManager.getLinkedinPage();
+
+        await linkedinPage.launchHomepage();
+        //await linkedinPage.ClickOnSigninoption();
+       // await linkedinPage.EnterEmail('erakki27@gmail.com');
+        //await linkedinPage.EnterPwd('Shutup@27a');
+        //await linkedinPage.ClickOnSignin();
         await linkedinPage.ClickOnProfile();
         await linkedinPage.ClickOnedit();
         await linkedinPage.SaveProfile();
